b30370202dd21f6405f1aa0ef82977cb2ac935c8
Thallison <misterthallison@gmail.com>
2023-07-30 22:30:30 -0300

    Adicionando os exercícios até o 10

---
+++ respostas_exercicios_sequencial/exercicio07.py
@@ -0,0 +1,5 @@
+lado = float(input("Quanto mede o lado do quadrado em centímetros? "))
+
+area_quadrado = (lado * lado)
+
+print("O dobro da área do quadrado é igual a: ", area_quadrado*2, "centímetros")
---
+++ respostas_exercicios_sequencial/exercicio10.py
@@ -0,0 +1,6 @@
+temperaturaC = float(input("Qual a temperatura em graus Celsius? "))
+
+temperaturaF = (temperaturaC*1.8)+32
+
+print(temperaturaC, "Graus Celsius convertidos em Graus Fareheint é igual a: ", temperaturaF)
+
